78d753d8b148681b1e01ec0768012fde81631882
Biswajit Debnath <biswajit.basanti@gmail.com>
2023-11-27 01:08:06 +0530

    fix: :art: image position changed

--- src/components/parralax/Parallax.jsx
+++ src/components/parralax/Parallax.jsx
@@ -1,9 +1,9 @@
 /* eslint-disable react/prop-types */
-import './parallax.scss'
+// import '../../style/components/parallax.scss'
 import { useRef } from "react";
 import { motion, useScroll, useTransform } from "framer-motion";
 
-const AboutParallax = ({title,section}) => {
+const AboutParallax = ({ title, section }) => {
   const ref = useRef();
   const { scrollYProgress } = useScroll({
     target: ref,
@@ -17,8 +17,8 @@ const AboutParallax = ({title,section}) => {
   );
   const textParallaxAnimation = useTransform(
     scrollYProgress,
-    [0,0.25, 0.5, 0.75, 1],
-    ["-300%","0%", "100%", "200%","280%"]
+    [0, 0.25, 0.5, 0.75, 1],
+    ["-300%", "0%", "100%", "200%", "280%"]
   );
   const textScaleAnimation = useTransform(
     scrollYProgress,
@@ -26,15 +26,21 @@ const AboutParallax = ({title,section}) => {
     [1, 1.2, 1.5] // Adjust the scaling values as needed
   );
 
-
   return (
     <div className="parallax" ref={ref}>
-      <motion.img
-        style={{ y: imageParallaxAnimation }}
-        src={section}
-        alt=""
-      />
-      <motion.div
+      <motion.img style={{ y: imageParallaxAnimation }} src={section} alt="" />
+      <div className="overlay">
+        <motion.div
+          className="title"
+          style={{
+            y: textParallaxAnimation,
+            scale: textScaleAnimation,
+          }}
+        >
+          {title}
+        </motion.div>
+      </div>
+      {/* <motion.div
         className="title"
         style={{
           y: textParallaxAnimation,
@@ -42,9 +48,9 @@ const AboutParallax = ({title,section}) => {
         }}
       >
         {title}
-      </motion.div>
+      </motion.div> */}
     </div>
   );
-}
+};
 
-export default AboutParallax
+export default AboutParallax;
